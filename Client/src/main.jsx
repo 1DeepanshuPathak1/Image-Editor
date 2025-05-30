ca2642da0b06fa5b0776c8d9b3338b048e75913f
Deepanshu Pathak <Deepanshu70.2010@gmail.com>
2025-05-30 15:41:42 +0530

    fixed few frontend bugs

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './app'; 
-import './css/EditPage.css';    
+import App from './app';    
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
